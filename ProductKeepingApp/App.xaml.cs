968dc78b0b1e9ce4415b57ce066bc7224bbd3c3e
OniDem1 <spaindecraft17@gmail.com>
2023-11-22 10:39:23 +0700

    Добавьте файлы проекта.

---
+++ ProductKeepingApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductKeepingApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProductKeepingApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProductKeepingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
